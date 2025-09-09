d6b973a728aea9f0e50ffed93408c65c14789180
sclau <saftaclaudia27@yahoo.com>
2025-09-09 11:19:27 +0300

    Add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,33 +1,44 @@
+import CookbookHome from "../assets/images/Cookbook-Home.png";
+import CookbookRecipes from "../assets/images/Cookbook-Recipes.png";
+import ArtHome from "../assets/images/ArtGallery-Home.png";
+import ArtAbout from "../assets/images/ArtGallery-About.png";
+import ArtGallery from "../assets/images/ArtGallery-Gallery.png";
+import ArtContact from "../assets/images/ArtGallery-Contact.png";
+
+import AdminArtLogin from "../assets/images/ArtAdmin-Login.png";
+import AdminArtAdd from "../assets/images/ArtAdmin-Add.png";
+import AdminArtGallery from "../assets/images/ArtAdmin-Gallery.png";
+
+import TodoLight from "../assets/images/Todo-Light.png";
+import TodoDark from "../assets/images/Todo-Dark.png";
+
 import ProjectCard from "./ProjectCard";
 
 export default function Projects() {
   const projectList = [
     {
       title: "Art Portfolio Website",
-      images: [
-        "/images/ArtGallery-Home.png",
-        "/images/ArtGallery-About.png",
-        "/images/ArtGallery-Gallery.png",
-        "/images/ArtGallery-Contact.png",
-      ],
+      images: [ArtHome, ArtAbout, ArtGallery, ArtContact],
       githubLink: "https://github.com/saftaclaudia/art-portfolio",
       tech: ["React", "Tailwind", "Framer Motion", "Supabase"],
     },
     {
       title: "Art Admin Portfolio",
-      images: [
-        "/images/ArtAdmin-Login.png",
-        "/images/ArtAdmin-Add.png",
-        "/images/ArtAdmin-Gallery.png",
-      ],
+      images: [AdminArtLogin, AdminArtAdd, AdminArtGallery],
       githubLink: "https://github.com/saftaclaudia/supabase-artworks",
       tech: ["React", "Tailwind", "Framer Motion", "Supabase"],
     },
     {
       title: "Cookbook Website",
-      images: ["/images/Cookbook-Home.png", "/images/Cookbook-Recipes.png"],
+      images: [CookbookHome, CookbookRecipes],
       githubLink: "https://github.com/saftaclaudia/recipe-book",
       tech: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      title: "To-do app",
+      images: [TodoLight, TodoDark],
+      githubLink: "https://saftaclaudia.github.io/todo-vite-react-ts-scss",
+      tech: ["React", "SCSS", "TypeScript"],
     },
   ];
   return (
